0127115cb4a93343181653b37b7a4bdb4a407269
KaioFFN <104567356+KaioFFN@users.noreply.github.com>
2022-11-16 15:43:33 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -17,19 +17,44 @@ public class Program
             switch (opção)
             {
                 case 1:
-                string data , nomeCliente , emailCliente;
-                Console.WriteLine("\t\t Digite a data desejada");
-                data = Console.ReadLine();
-                Console.WriteLine("\t\t Nome do cliente");
-                nomeCliente = Console.ReadLine();
-                Console.WriteLine("\t\t Email cliente");
-                emailCliente = Console.ReadLine();
-                 Cliente cliente = new Cliente(nomeCliente , emailCliente);
-                restaurante.ReservarMesa(quantMesa , data , ref cliente);
-                quantMesa++;
+                    string data , nomeCliente , emailCliente;
+                    Console.WriteLine("\t\t Digite a data desejada");
+                    data = Console.ReadLine();
+                    Console.WriteLine("\t\t Nome do cliente");
+                    nomeCliente = Console.ReadLine();
+                    Console.WriteLine("\t\t Email cliente");
+                    emailCliente = Console.ReadLine();
+                     Cliente cliente = new Cliente(nomeCliente , emailCliente);
+                    restaurante.ReservarMesa(quantMesa , data , ref cliente);
+                    quantMesa++;
                 break;
                 case 2:
-                break;
+                    int mesaE , quantidade , buffer , tipo;
+                    string item  ;
+                    double valor;
+                    Console.WriteLine("\t\t Digite a mesa desejada");
+                    mesaE = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\t\t Digite o item desejado");
+                    item = Console.ReadLine();
+                    Console.WriteLine("\t\t Digite o valor do item");
+                    valor = double.Parse(Console.ReadLine());
+                    Console.WriteLine("\t\t Digite a quantidade");
+                    quantidade = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\t\t Digite o tipo desejado \n\t\t1 = comida\n\t\t0 = bebida");
+                    tipo = int.Parse(Console.ReadLine());
+                    if (tipo == 0)
+                    {
+                        restaurante.Mesas[mesaE].AdicionarNaComanda(ComandaTipo.Bebida, item, valor, quantidade);
+                    }
+                    else if (tipo == 1)
+                    {
+                        restaurante.Mesas[mesaE].AdicionarNaComanda(ComandaTipo.Comida, item, valor, quantidade);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Item invalido");
+                    }
+                    break;
             }
         } while (opção != 0);
     }
